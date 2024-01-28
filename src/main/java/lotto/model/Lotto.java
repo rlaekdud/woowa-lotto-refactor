74f7164191641ef9cceaf7fa78c1be18e52de070
rlaekdud <gloria030114@gmail.com>
2024-01-28 13:49:38 +0900

    Fix: Modify lotto object
    - 조건에 맞지 않게 기존에 생성했던 것을 조건에 맞춰 수정
    - Rank를 별도로 관리하도록 함

--- src/main/java/lotto/model/Lotto.java
+++ src/main/java/lotto/model/Lotto.java
@@ -4,22 +4,20 @@ import java.util.List;
 
 public class Lotto {
 
-    List<Integer> lottoNumbers;
-    Rank rank;
+    private final List<Integer> numbers;
 
-    public Lotto(List<Integer> lottoNumbers) {
-        this.lottoNumbers = lottoNumbers;
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+        this.numbers = numbers;
     }
 
-    public List<Integer> getLottoNumbers() {
-        return lottoNumbers;
+    private void validate(List<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public Rank getRank() {
-        return rank;
-    }
-
-    public void setRank(Rank rank) {
-        this.rank = rank;
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 }
