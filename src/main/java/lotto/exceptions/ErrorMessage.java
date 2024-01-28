c96ee8d9425c6f94e252c6b38601fdd47520efd9
rlaekdud <gloria030114@gmail.com>
2024-01-28 14:26:45 +0900

    Feat: Add "cannot divide by thousand" ErrorMessage
    - 구입 금액을 validate하는 과정에서 발생하는 에러 추가
    - 1000원 단위로 입력을 받아야 하기 때문에 에러 문구 추가

--- src/main/java/lotto/exceptions/ErrorMessage.java
+++ src/main/java/lotto/exceptions/ErrorMessage.java
@@ -4,7 +4,8 @@ public enum ErrorMessage {
     NUM_OUT_OF_RANGE("[ERROR] 로또 숫자의 범위는 1~45여야 합니다."),
     DUPLICATED_NUM("[ERROR] 로또 숫자는 중복되지 않아야 합니다."),
     INVALID_NUM_COUNT("[ERROR] 로또 숫자는 6개여야 합니다."),
-    NOT_NUM_INPUT("[ERROR] 입력 형식이 올바르지 않습니다.");
+    NOT_NUM_INPUT("[ERROR] 입력 형식이 올바르지 않습니다."),
+    CANNOT_DIVIDE_BY_THOUSAND("[ERROR] 구입금액은 1000원 단위로 입력되어야 합니다.");
 
     private String message;
 
