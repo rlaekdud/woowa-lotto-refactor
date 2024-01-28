49ea119ce5c0f12e5f6ada1735b48b15ddd9f090
rlaekdud <gloria030114@gmail.com>
2024-01-28 13:35:11 +0900

    Feat: Add input format error message
    - 입력 형식이 올바르지 않은 경우 예외 처리 및 문구 작성

--- src/main/java/lotto/exceptions/ErrorMessage.java
+++ src/main/java/lotto/exceptions/ErrorMessage.java
@@ -3,7 +3,8 @@ package lotto.exceptions;
 public enum ErrorMessage {
     NUM_OUT_OF_RANGE("[ERROR] 로또 숫자의 범위는 1~45여야 합니다."),
     DUPLICATED_NUM("[ERROR] 로또 숫자는 중복되지 않아야 합니다."),
-    INVALID_NUM_COUNT("[ERROR] 로또 숫자는 6개여야 합니다.");
+    INVALID_NUM_COUNT("[ERROR] 로또 숫자는 6개여야 합니다."),
+    NOT_NUM_INPUT("[ERROR] 입력 형식이 올바르지 않습니다.");
 
     private String message;
 
